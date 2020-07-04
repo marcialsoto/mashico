04cbdf3cc1c4ab4b24c0a9aafbc156fb97422e46
Marcial Soto <marcialsotosanchez@gmail.com>
2020-07-04 02:23:35 -0500

    Primero commit

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Mashico
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-footer-wrapper">
+			<nav id="site-footer-navigation" class="footer-navigation">
+				<a href="https://dribbble.com/marcialsoto" target="_blank">
+					<?php echo do_shortcode( '[icon type="dribbble"]' ); ?>
+				</a>
+				<a href="https://www.linkedin.com/in/marcialsoto/" target="_blank">
+					<?php echo do_shortcode( '[icon type="linkedin"]' ); ?>
+				</a>
+				<a href="https://www.facebook.com/marcialsotocom" target="_blank">
+					<?php echo do_shortcode( '[icon type="facebook"]' ); ?>
+				</a>
+				<a href="https://www.youtube.com/channel/UCmNQkmawK1SOsgrNopgFWcw" target="_blank">
+					<?php echo do_shortcode( '[icon type="youtube"]' ); ?>
+				</a>
+			</nav><!-- #site-navigation -->
+			<div class="site-info">
+				<?php 
+					$year = date("Y");
+					
+					echo $year .' @ <a href="'. get_bloginfo('url') .'">'. get_bloginfo('name') .'</a>';
+				?>
+			</div><!-- .site-info -->
+			
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Mashico functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Mashico
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+if ( ! function_exists( 'mashico_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function mashico_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Mashico, use a find and replace
+		 * to change 'mashico' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'mashico', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'mashico' ),
+			)
+		);
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'mashico_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'mashico_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function mashico_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'mashico_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'mashico_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function mashico_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'mashico' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'mashico' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'mashico_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function mashico_scripts() {
+	wp_enqueue_style( 'mashico-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_style_add_data( 'mashico-style', 'rtl', 'replace' );
+
+	wp_enqueue_script( 'mashico-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'mashico_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
+add_shortcode( 'icon', 'mrov_icon_func' );
+function mrov_icon_func( $atts ) {
+    $atts = shortcode_atts( array(
+        'type' => 'twitter',
+        'theme' => 'fill',
+	), $atts, 'icon' );
+
+	$result = '';
+
+	$result .= '<i class="icon icon-'. $atts['type'] .'">';
+		$result .= '<svg width="1em" height="1em" fill="currentColor" stroke="currentColor">';
+			$result .= '<use xlink:href="'. get_template_directory_uri() .'/images/svg-defs.svg#icon-' .$atts['type'].'-'. $atts['theme'] .'" />';
+		$result .= '</svg>';
+	$result .= '</i>';
+ 
+    return $result;
+}
+
